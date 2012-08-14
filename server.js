1c9e844737dde1ca415fc39779b61c08fdbf038f
Julian Gautier <julian.gautier@alumni.neumont.edu>
2012-08-14 09:57:57 -0700

    shit is working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs')
 var currentBoard;
+var isOn = true;
 var soundPin = 3;
 var servoPin = 5;
 var rgb = "FF0000";
@@ -13,19 +14,49 @@ var port = process.env.NODE_PORT || 9892;
 var host = process.env.NODE_HOST || '0.0.0.0';
 var util = require('util');
 var routes = {
+	'/' : serveHTML,
+	'/on' : on,
+	'/off' : off,
 	'/siren': siren,
 	'/color':color,
-	'/tone':tone,
-	'/rotate':rotate,
-	'/test':test
+	'/tone':tone
 };
 
-function color (){
-
+function on() {
+	isOn = true;
+	console.log("turning on");
+	res.statusCode = 200;
+	res.end();
 }
 
-function tone () {
+function off() {
+	isOn = false;
+	console.log("turning off");
+	res.statusCode = 200;
+	res.end();
+}
 
+function serveHTML(req, res, body) {
+ // Route it
+	fs.readFile("./arduino.html", function (err, data) {
+		if (err) {
+			res.statusCode = 404;
+			return res.end();
+		}
+		res.end(data);
+	});
+}
+
+function color (req, res, body){
+	rgb = body;
+	res.statusCode = 200;
+	res.end();
+}
+
+function tone (req, res, body) {
+	soundFrequency = body;
+	res.statusCode = 200;
+	res.end();
 }
 
 function rotate () {
@@ -36,54 +67,59 @@ function test () {
 
 }
 
-function siren () {
-	console.log(currentBoard);
-	var board = currentBoard;
-	var servoIntervalId;
-	var degrees = 0;
-	console.log(board);
-	if(servoSpeed) {
-		board.pinMode(servoPin,board.MODES.SERVO);
-		board.servoWrite(servoPin,10);
-		servoIntervalId = setInterval(function(){
-			if(degrees >= 180){
-				board.servoWrite(servoPin, 10);
-				degrees = 0;
-			} else {
-				board.servoWrite(servoPin, 180);
-				degrees = 180;
+function siren (req, res, body) {
+	res.statusCode = 200;
+	res.end();
+	if(isOn) {
+		console.log(currentBoard);
+		var board = currentBoard;
+		var servoIntervalId;
+		var degrees = 0;
+		console.log(board);
+		if (servoSpeed) {
+			console.log("running servo");
+			board.pinMode(servoPin,board.MODES.SERVO);
+			board.servoWrite(servoPin,20);
+			servoIntervalId = setInterval(function(){
+				if(degrees >= 160){
+					board.servoWrite(servoPin, 20);
+					degrees = 20;
+				} else {
+					board.servoWrite(servoPin, 160);
+					degrees = 160;
+				}
+			},servoSpeed);
+		}
+
+		if(rgb != "FFFFFF") {
+			var commandArray = [0x6e];
+			for(var i = 0; i < 6; i +=2) {
+				commandArray.push("0x" + rgb[i] + rgb[i+1]);
 			}
-		},servoSpeed);
-	}
-
-	if(rgb != "FFFFFF") {
-		var commandArray = [0x6e];
-		for(var i = 0; i < 6; i +=2) {
-			commandArray.push("0x" + rgb[i] + rgb[i+1]);
-		}
-		board.sendI2CWriteRequest(0x09, commandArray);
-	}
-
-	if(soundFrequency) {
-		board.analogWrite(soundPin, soundFrequency);
-	}
-
-	setTimeout(function () {
-		if(servoIntervalId) {
-			clearInterval(servoIntervalId);
+			board.sendI2CWriteRequest(0x09, commandArray);
 		}
 
-		//set color to FFFFFF
-		board.sendI2CWriteRequest(0x09, [0x6e, 0x00, 0x00, 0x00]);
-		//set sound to 0;
-		board.analogWrite(soundPin, 0);
+		if(soundFrequency) {
+			board.analogWrite(soundPin, soundFrequency);
+		}
 
-	}, sirenDuration);
+		setTimeout(function () {
+			if(servoIntervalId) {
+				clearInterval(servoIntervalId);
+				board.pinMode(servoPin, board.MODES.INPUT);
+			}
+
+			//set color to FFFFFF
+			board.sendI2CWriteRequest(0x09, [0x6e, 0x00, 0x00, 0x00]);
+			//set sound to 0;
+			board.analogWrite(soundPin, 0);
+
+		}, sirenDuration);
+	}
 }
 
-/*var server = firmata.createServer(function(board){
+var server = firmata.createServer(function(board){
 	currentBoard = board;
-	board.pinMode(servoPin, board.MODES.SERVO);
 	board.pinMode(soundPin, board.MODES.PWM);
 	board.sendI2CConfig();
 	board.sendI2CWriteRequest(0x09, [0x6f]);
@@ -92,7 +128,6 @@ function siren () {
 server.listen(3030,function(){
 	console.log('listening');
 });
-*/
 
 http.createServer(function(req, res) {
     var uri, body;
@@ -122,29 +157,14 @@ http.createServer(function(req, res) {
 
     // Check post data
     body = '';
-    if (req.method === 'POST') {
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
-        req.on('end', function() {
-            // Route it
-            body = (body) ? querystring.parse(body) : null;
-            return route(req, res, body);
-        });
-    } else {
+    req.on('data', function(chunk) {
+        body += chunk;
+    });
+    req.on('end', function() {
         // Route it
-       fs.readFile("./arduino.html", function (err, data) {
-      if (err) {
-        res.statusCode = 404
-        return res.end()
-      }
-
-      res.end(data)
-    })
-		//res.end('fuck')
-        return route(req, res);
-    }
-
+        //body = (body) ? querystring.parse(body) : null;
+        return route(req, res, body);
+    });
 
 }).listen(port, host, function() {
     console.log('Server started on http://%s:%d', host, port);
